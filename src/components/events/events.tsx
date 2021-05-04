97d7762fd57e67a1a86b18e6d6099bc92ca3bce8
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-05-04 22:20:20 +0300

    Updated events component

--- src/components/events/events.tsx
+++ src/components/events/events.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { styledButton, styledEvents } from './styled';
 
 const Events = () => {
-  return <h1>Events Page</h1>;
+  return (
+    <div className={styledEvents}>
+      <p>Share your own Events!</p>
+      <button className={styledButton}>Create Event</button>
+    </div>
+  );
 };
 
 export default Events;
---
+++ src/components/events/styled.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/css';
+
+export const styledEvents = css`
+  width: 30rem;
+  max-width: 80%;
+  margin: 2rem auto;
+  padding: 3rem;
+  text-align: center;
+  border: 1px solid #01d1d1;
+`;
+
+export const styledButton = css`
+  font: inherit;
+  color: #000000;
+  background: #01d1d1;
+  border: none;
+  cursor: pointer;
+
+  &:hover,
+  &:active,
+  &.active {
+    color: #fdefa0;
+  }
+`;
