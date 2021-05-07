d5a1a060d0baba2f4076135cdfa728213cd39785
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-05-08 00:26:15 +0300

    Added graphql variables

--- src/components/auth/auth.tsx
+++ src/components/auth/auth.tsx
@@ -18,26 +18,34 @@ const Auth = () => {
 
     let requestBody = {
       query: `
-        query {
-          login(email: "${email}", password: "${password}") {
+        query Login($email: String!, $password: String!) {
+          login(email: $email, password: $password) {
             userId
             token
             tokenExpiration
           }
         }
       `,
+      variables: {
+        email: email,
+        password: password,
+      },
     };
 
     if (!isLogin) {
       requestBody = {
         query: `
-          mutation {
-            createUser(userInput: {email: "${email}", password: "${password}"}) {
+          mutation CreateUser($email: String!, $password: String!) {
+            createUser(userInput: {email: $email, password: $password}) {
               _id
               email
             }
           }
         `,
+        variables: {
+          email: email,
+          password: password,
+        },
       };
     }
 
--- src/components/bookings/bookings.tsx
+++ src/components/bookings/bookings.tsx
@@ -18,13 +18,16 @@ const Bookings = () => {
 
     const requestBody = {
       query: `
-          mutation {
-            cancelBooking(bookingId: "${bookingId}") {
+          mutation CancelBooking($id: ID!) {
+            cancelBooking(bookingId: $id) {
               _id
               title
             }
           }
         `,
+      variables: {
+        id: bookingId,
+      },
     };
 
     fetch('http://localhost:8080/graphql', {
