574c8d2b0529351fe94012e8bf7420b898f6c9d1
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-05-18 20:49:33 +0300

    Updated backdrop component styles

--- src/components/backdrop/styled.ts
+++ src/components/backdrop/styled.ts
@@ -6,6 +6,7 @@ export const styledBackdrop = css`
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgb(0 0 0 / 0.75);
+  background: rgb(47 47 47 / 0.5);
   z-index: 10;
+  backdrop-filter: blur(7px);
 `;
