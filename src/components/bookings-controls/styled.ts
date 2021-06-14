7f264ca12441bab71e4c3ef388e70a5ff5c6741f
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-06-14 12:39:31 +0300

    Updated bookings

--- src/components/bookings-controls/styled.ts
+++ src/components/bookings-controls/styled.ts
@@ -11,15 +11,10 @@ export const styles = {
   `,
   button: css`
     padding: 16px 24px;
-    font: inherit;
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 16px;
     color: #282828;
     background-color: #ffffff;
     border: 1px solid #ef4723;
     border-radius: 32px;
-    cursor: pointer;
 
     &:hover,
     &:focus {
