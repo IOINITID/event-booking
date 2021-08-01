42c8ddc5098a8eb12339486e202796049bf79d51
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-08-01 13:08:14 +0300

    Updated button component type

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,27 +2,22 @@
 import { styles } from './styles';
 
 // Types imports
-import { ButtonType } from './types';
+import { ButtonProps } from './types';
 
 // Additional imports
 import { cx } from '@emotion/css';
-import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
-
-type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & ButtonType;
 
 const Button = ({ className, variant = 'text', children, ...props }: ButtonProps) => {
+  const buttonStyles = cx(
+    styles.root,
+    variant === 'text' && styles.text,
+    variant === 'outlined' && styles.outlined,
+    variant === 'contained' && styles.contained,
+    className
+  );
+
   return (
-    <button
-      className={cx(
-        styles.root,
-        variant === 'text' && styles.text,
-        variant === 'outlined' && styles.outlined,
-        variant === 'contained' && styles.contained,
-        className
-      )}
-      type="button"
-      {...props}
-    >
+    <button className={buttonStyles} type="button" {...props}>
       {children}
     </button>
   );
--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -1,3 +1,5 @@
-export type ButtonType = {
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+
+export type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
   variant?: 'text' | 'outlined' | 'contained';
 };
