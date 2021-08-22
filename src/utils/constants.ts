5c68712c18dca0d979e53188681894009dfa19d8
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-08-22 19:30:24 +0300

    Updated constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-// export const REQUEST_URL = 'https://ioinitid-event-booking.herokuapp.com/graphql';
-export const REQUEST_URL = 'http://localhost:8080/graphql';
+export const REQUEST_URL = 'https://ioinitid-event-booking.herokuapp.com/graphql';
+// export const REQUEST_URL = 'http://localhost:8080/graphql';
